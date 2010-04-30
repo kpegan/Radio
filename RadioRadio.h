270f0c0e594ba3adf8ebadb8f3cc3ea625fc0206
Kelly Egan <kelly@egan.net>
2010-04-30 09:43:44 -0400

    Initial commit

---
+++ RadioRadio.h
@@ -0,0 +1,26 @@
+/*
+ Radio.h
+ Radio
+ 
+ Created by Kelly Egan on 4/30/10.
+ 
+ This library is indebted to the RF12 library created by Jean-Claude Wippler @ 
+ Jee labs http://jeelaps.org It is substantially different than the original 
+ library particularly in packet structure and that it is now a C++ class. I also
+ attempted to additionally comment the source code mostly for my own understanding.
+ 
+ The MIT License
+ 
+ Copyright (c) <2010> Kelly Egan
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ */
