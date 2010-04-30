a1c620f9a332358eda7dab099103cccbe5f01956
Kelly Egan <kelly@egan.net>
2010-04-30 11:00:45 -0400

    Radio class def added, begin() and SPIcmd() added

--- Radio.cpp
+++ Radio.cpp
@@ -8,5 +8,82 @@
  
 */
 
-#include "RadioRadio.h"
+#include "Radio.h"
+
+
+//Radio class PUBLIC METHODS
+
+Radio::Radio(char freq, char grp, char node) {
+    _frequency = freq;
+    _group = grp;
+    _nodeID = node;
+}
+
+void Radio::begin() {
+    //Initialize SPI communications pins for RFM12B
+	pinMode(SPI_MOSI, OUTPUT);     
+	pinMode(SPI_MISO, INPUT);
+	pinMode(SPI_SCK, OUTPUT);
+	pinMode(SPI_SS, OUTPUT); 
+    
+    //Disable communications
+	digitalWrite(SPI_SS, HIGH);  
+	
+	//IF Clock speed of Arduino doesn't exceed 10Mhz
+#if F_CPU <= 10000000
+	// clk/4 is ok for the RF12's SPI
+	SPCR = _BV(SPE) | _BV(MSTR);
+#else
+	// use clk/8 (2x 1/16th) to avoid exceeding RF12's SPI specs of 2.5 MHz
+	SPCR = _BV(SPE) | _BV(MSTR) | _BV(SPR0);
+	SPSR |= _BV(SPI2X);
+#endif
+
+    //Initialize interrupt pin
+    pinMode(RFM_IRQ, INPUT);
+    digitalWrite(RFM_IRQ, 1); //pull-up
+    
+    //Begin initialization of Radio
+    SPIcmd(0x0000);
+    SPIcmd(RF_SLEEP_MODE);
+    
+    //This while loop seems,on occasion to hang. Why?
+    //Serial.println("Waiting...");
+    while (digitalRead(RFM_IRQ) == 0)
+        SPIcmd(0x0000);
+    //Serial.println("Radio awake...");
+    
+    //Details of these commands can be found in the Command Reference
+    SPIcmd(0x80C7 | (_frequency << 4)); // EL (enable TX), EF (enable RX FIFO), 12.0pF 
+    SPIcmd(0xA640); // Frequency setting command 
+    SPIcmd(0xC606); // Data Rate Command - 57.6Kbps (38.4: 8, 19.2: 11, 9.6: 23, 4.8: 47)
+    SPIcmd(0x94A2); // Receiver Control Command - VDI,FAST,134kHz,0dBm,-91dBm 
+    SPIcmd(0xC2AC); // Data Filter Command - AL,!ml,DIG,DQD4 
+    SPIcmd(0xCA83); // FIFO and Reset Mode Command - FIFO8,SYNC,!ff,DR 
+    SPIcmd(0xCE00 | _group); // Syncron pattern command - SYNC=2DXX； 
+    SPIcmd(0xC483); // @PWR,NO RSTRIC,!st,!fi,OE,EN 
+    SPIcmd(0x9850); // !mp,90kHz,MAX OUT 
+    SPIcmd(0xCC77); // OB1，OB0, LPX,！ddy，DDIT，BW0 
+    SPIcmd(0xE000); // Wake-Up Timer Command - NOT USED
+    SPIcmd(0xC800); // Low Duty-Cycle Command - NOT USED 
+    SPIcmd(0xC049); // Low Battery Detector and Microcontroller Clock Divider Command - 1.66MHz,3.1
+    
+}
+
+uint16_t Radio::SPIcmd(uint16_t cmd) {
+    uint16_t reply;
+    digitalWrite(SPI_SS, 0);
+    SPDR = cmd >> 8;
+    while (!(SPSR & _BV(SPIF)))
+        ;
+    reply = SPDR << 8;
+    SPDR = cmd;
+    while (!(SPSR & _BV(SPIF)))
+        ;
+    reply |= SPDR;
+    digitalWrite(SPI_SS, 1);
+    return reply;
+}
+
+//Radio class PRIVATE METHODS
 
--- Radio.h
+++ Radio.h
@@ -5,7 +5,7 @@
  Created by Kelly Egan on 4/30/10.
  
  This library is indebted to the RF12 library created by Jean-Claude Wippler @ 
- Jee labs http://jeelaps.org It is substantially different than the original 
+ Jee labs http://jeelaps.org It is substantially different than the his 
  library particularly in packet structure and that it is now a C++ class. I also
  attempted to additionally comment the source code mostly for my own understanding.
  
@@ -23,4 +23,54 @@
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
  
- */
+*/
+
+#ifndef Radio_h
+#define Radio_h
+
+#include "stdint.h"
+#include "WProgram.h"
+#include <util/crc16.h>
+
+//Arduino pins for RFM12B communication (RFM12B pin name)
+#define RFM_IRQ  0    //0 is the first interupt, Arduino pin 2 (nIRQ)
+#define SPI_SS   10   //Chip select. Pull low to send signal to RF module (nSEL)
+#define SPI_MOSI 11   //Data out from Arduino (SDI)
+#define SPI_MISO 12   //Data input from RFM12B (SDO)
+#define SPI_SCK  13   //Clock input (SCK)
+
+//Commands given to RFM12B via SPI
+#define RF_RECEIVER_ON  0x82DD  // Turn on receiver (See Power Management Command, RF12B Program Guide)
+#define RF_XMITTER_ON   0x823D  // Turn on transmitter (See Power Management Command, RF12B Program Guide)
+#define RF_IDLE_MODE    0x820D  // See Power Management Command, RF12B Program Guide
+#define RF_SLEEP_MODE   0x8205  // See Power Management Command, RF12B Program Guide
+#define RF_WAKEUP_MODE  0x8207  // Enable wakeup timer (See Power Management Command, RF12B Program Guide)
+#define RF_TXREG_WRITE  0xB800  // Send a char (See Transmitter Register Write Command, RF12B Program Guide)
+#define RF_RX_FIFO_READ 0xB000  // See Receiver FIFO Read command, RF12B Program Guide
+#define RF_WAKEUP_TIMER 0xE000  // See WakeUp Command, RF12B Program Guide
+
+//RFM12B carrier frequencies
+#define RF12_433MHZ     1
+#define RF12_868MHZ     2
+#define RF12_915MHZ     3   //Used in the North and South America
+
+
+class Radio
+{
+public:
+    Radio(char freq, char grp, char node); 
+	void begin();  //Initialize the radio
+    void send(char destination, char *message);  //Send a message
+    //void send(char destination, char *data, boolean anon);   //Send a message anonymously
+    boolean received(); //Has a message been received?
+    void read(char *message); //Read the message into char array at message
+    
+    uint16_t SPIcmd(uint16_t cmd);      //give command to RFM12B via SPI
+    
+private:
+	uint8_t _frequency;            //Carrier frequency: RF12_433MHZ, RF12_868MHZ or RF12_915MHZ 
+	uint8_t _group;                //Network group 212 (0xD4) is default.
+	uint8_t _nodeID;               //Node ID can be 1-31
+};
+
+#endif
